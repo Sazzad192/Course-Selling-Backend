e10cddfb576e076541f32ea04070ed24093a0edb
K.M. SAZZADUL ISLAM <18-37693-1@student.aiub.edu>
2022-10-27 13:46:24 +0600

    data loaded

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 
 const categories= require('./data/categories.json');
+const courses = require('./data/courses.json')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -15,7 +16,30 @@ app.get('/categories', (req, res)=>{
     res.send(categories)
 })
 
+app.get('/categories/:id', (req, res) =>{
+    const id = req.params.id;
 
+    if(id === '03'){
+      const fullstack = courses.filter(course => course.cat_id === "01" || course.cat_id === "02");
+      res.send(fullstack);
+    }
+    else{
+      const course = courses.filter(course => course.cat_id === id);
+      
+      res.send(course);
+    }
+    
+})
+
+app.get('/details', (req, res)=>{
+  res.send(courses)
+})
+
+app.get('/details/:id', (req, res)=>{
+    const id = req.params.id;
+    const selected= courses.find(n => n.id === id);
+    res.send(selected)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
